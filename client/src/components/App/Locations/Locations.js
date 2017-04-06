d211e923e8d5986113230a010964895104b47252
Palmer <palmm@users.noreply.github.com>
2017-04-06 01:14:21 -0400

    finished location page

--- client/src/components/App/Locations/Locations.js
+++ client/src/components/App/Locations/Locations.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Panel, Table, Col} from 'react-bootstrap';
 
 export default class Locations extends Component {
 
@@ -24,8 +25,45 @@ export default class Locations extends Component {
 	}
 
 	render() {
+		let locations = this.state.locations;
+		let tableData = [];
+
+		for(var location in locations) {
+			tableData.push(
+				<tr key={location}>
+					<td>{ locations[location].StreetNum }</td>
+					<td>{ locations[location].StreetName }</td>
+					<td>{ locations[location].City }</td>
+					<td>{ locations[location].Province }</td>
+					<td>{ locations[location].PostalCode }</td>
+					<td>{ locations[location].NumSpaces }</td>
+				</tr>
+			);
+		}
+
 		return (
-			<h1>Locations</h1>
+			<Col mdOffset={2} md={8}>
+				<Panel>
+					<h1>Locations</h1>
+					<h4>Find all the locations for KTCS below.</h4>
+
+					<Table striped bordered condensed hover>
+						<thead>
+							<tr>
+								<th>Street Number</th>
+								<th>Street Name</th>
+								<th>City</th>
+								<th>Province</th>
+								<th>Postal Code</th>
+								<th>Number of Spaces</th>
+							</tr>
+						</thead>
+						<tbody>
+							{ tableData }
+						</tbody>
+					</Table>
+				</Panel>
+			</Col>
 		);
 	}
 
