0ea2e6f4d44ff28064bdebb2893276dffc4a6e29
zharley18 <zackharley@gmail.com>
2017-04-06 04:06:50 -0400

    Rental history

--- client/src/components/App/Rentals/Rentals.js
+++ client/src/components/App/Rentals/Rentals.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from'axios';
-import { Grid, Row, Col, Table } from 'react-bootstrap';
+import { Col, Panel, Table } from 'react-bootstrap';
 import auth from '../../../util/auth';
 
 export default class Rentals extends Component {
@@ -28,41 +28,43 @@ export default class Rentals extends Component {
 
 	render() {
 		return (
-			<section>
-				<h1>Rentals</h1>
-				<Grid>
-					<Row>
-						<Col>
-							<Table bordered>
-								<thead>
-									<tr>
-										<th>Car Make</th>
-										<th>Car Model</th>
-										<th>Car Year</th>
-										<th>Pick Up Date</th>
-										<th>Drop Off Date</th>
+			<Col mdOffset={2} md={8}>
+				<Panel>
+					<h1>My Rental History</h1>
+					<h4>These are all of your rentals.</h4>
+
+					<Table striped bordered condensed hover>
+						<thead>
+							<tr>
+								<th>Reservation ID</th>
+								<th>Access Code</th>
+								<th>Car Make</th>
+								<th>Car Model</th>
+								<th>Car Year</th>
+								<th>Pick Up Date</th>
+								<th>Drop Off Date</th>
+							</tr>
+						</thead>
+						<tbody>
+							{this.state.rentals.map((rental) => {
+								const pickUpDate = new Date(rental.PickupDate);
+								const dropOffDate = new Date(rental.DropOffDate);
+								return (
+									<tr key={rental.ReservationID}>
+										<td>{rental.ReservationID}</td>
+										<td>{rental.AccessCode}</td>
+										<td>{rental.Make}</td>
+										<td>{rental.Model}</td>
+										<td>{rental.Year}</td>
+										<td>{pickUpDate.toLocaleDateString()}</td>
+										<td>{dropOffDate.toLocaleDateString()}</td>
 									</tr>
-								</thead>
-								<tbody>
-									{this.state.rentals.map((rental) => {
-										const pickUpDate = new Date(rental.PickupDate);
-										const dropOffDate = new Date(rental.DropOffDate);
-										return (
-											<tr key={rental.ReservationID}>
-												<td>{rental.Make}</td>
-												<td>{rental.Model}</td>
-												<td>{rental.Year}</td>
-												<td>{pickUpDate.toLocaleDateString()}</td>
-												<td>{dropOffDate.toLocaleDateString()}</td>
-											</tr>
-										);
-									})}
-								</tbody>
-							</Table>
-						</Col>
-					</Row>
-				</Grid>
-			</section>
+								);
+							})}
+						</tbody>
+					</Table>
+				</Panel>
+			</Col>
 		);
 	}
 
